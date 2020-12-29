c4586fda096cde86c49343f1e480c079bcb606f0
Billy Martin <Billymartin910@gmail.com>
2020-12-29 01:26:30 +0000

    added role & policy

--- class1/group.tf
+++ class1/group.tf
@@ -7,7 +7,7 @@ resource "aws_iam_group_membership" "developer_team" {
   name = "developer-group-membership"
 
   users = [
-    "${aws_iam_user.jeff.name}"
+    "${aws_iam_user.jeff.name}",
   ]
 
   group = "${aws_iam_group.developer.name}"
