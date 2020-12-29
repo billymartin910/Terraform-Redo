e7ee3536cd4350497b0218b7793a76ae70e3abf1
Billy Martin <Billymartin910@gmail.com>
2020-12-29 18:28:20 +0000

    added key

--- practice2/instance.tf
+++ practice2/instance.tf
@@ -1,4 +1,6 @@
 resource "aws_instance" "web" {
   ami           = "ami-02354e95b39ca8dec"
   instance_type = "t2.micro"
+  associate_public_ip_address = true
+  key_name = "${aws_key_pair.deployed.key_name}"
 }
