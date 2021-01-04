b59a4e4a5d1a1ea02542a873e109629f3671affb
Billy Martin <Billymartin910@gmail.com>
2021-01-04 01:54:56 +0000

    added elb

--- Developer/asg_module.tf
+++ Developer/asg_module.tf
@@ -5,25 +5,22 @@ module "webapp" {
   max_size         = "1"
   desired_capacity = "1"
   image_owner      = "137112412989"
-  security_group_names = "aws_security_group.asg-sec-group.name"
 }
 
-module "python" {
-  source           = "../class4"
-  region           = "us-east-1"
-  min_size         = "1"
-  max_size         = "1"
-  desired_capacity = "1"
-  image_owner      = "137112412989"
-  security_group_names = "aws_security_group.asg-sec-group.name"
-}
+# module "python" {
+#   source           = "../class4"
+#   region           = "us-east-1"
+#   min_size         = "1"
+#   max_size         = "1"
+#   desired_capacity = "1"
+#   image_owner      = "137112412989"
+# }
 
-module "python-eu" {
-  source           = "../class4"
-  region           = "us-east-1"
-  min_size         = "1"
-  max_size         = "1"
-  desired_capacity = "1"
-  image_owner      = "137112412989"
-  security_group_names = "aws_security_group.asg-sec-group.name"
-}
+# module "python-eu" {
+#   source           = "../class4"
+#   region           = "us-east-1"
+#   min_size         = "1"
+#   max_size         = "1"
+#   desired_capacity = "1"
+#   image_owner      = "137112412989"
+# }
