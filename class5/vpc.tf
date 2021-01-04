abdfdf1c73cdb86af05be88c5d3025f94db85735
Billy Martin <Billymartin910@gmail.com>
2021-01-04 21:13:58 +0000

    map public ip on launch

--- class5/vpc.tf
+++ class5/vpc.tf
@@ -22,14 +22,17 @@ resource "aws_subnet" "subnet3" {
 resource "aws_subnet" "subnet101" {
   vpc_id     = "${aws_vpc.main.id}"
   cidr_block = "10.0.101.0/24"
+  map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "subnet102" {
   vpc_id     = "${aws_vpc.main.id}"
   cidr_block = "10.0.102.0/24"
+  map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "subnet103" {
   vpc_id     = "${aws_vpc.main.id}"
+  map_public_ip_on_launch = true
   cidr_block = "10.0.103.0/24"
 }
