348bbcc1c831d01ea2c7c1bcb23506b16d241d60
Billy Martin <Billymartin910@gmail.com>
2021-01-16 17:52:27 +0000

    added s3 & https module

--- udemy/module.tf
+++ udemy/module.tf
@@ -14,3 +14,11 @@
 # module "mymodule" {
 #     source = "github.com/in4it/terraform-modules-example-dont-use-this.git"
 # }
+
+# module "mymodule-over-https" {
+#     source = "https://example.com/mymodule.zip"
+# }
+
+# module "mymodule-in-s3" {
+#     source = "s3:https://s3-eu-west-1.amazonaws.com/examplecorp-terraform-modules/mymodule.zip"
+# }
