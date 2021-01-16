821576d9fe8d6f67b9466bc22ba3ef07fa171ca6
Billy Martin <Billymartin910@gmail.com>
2021-01-16 17:45:47 +0000

    added module

---
+++ udemy/module.tf
@@ -0,0 +1,8 @@
+# module "consul" {
+#     source = "hasicorp/consul/aws"
+#     verison = "0.1.0"
+# }
+
+# module "mymodule" {
+#     source = "./mymodule" # refers to a local path 
+# }
--- udemy/provider.tf
+++ udemy/provider.tf
@@ -1,20 +1,18 @@
 # provider "aws" {
 #     region = "us-east-1"
 # }
-
 # provider "aws" {
 #     alias = "eu"
 #     region = "eu-west-1"
 # }
-
 # resource "aws_instance" "my_instance" {
 #     provider = aws.eu
 #     [...]
 # }
-
 # module "mymodule" {
 #     source = "./mymodule"
 #     providers = {
 #         aws = aws.eu
 #     }
 # }
+
